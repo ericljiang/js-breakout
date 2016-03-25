75f8bb686cb17e8abdb08319dcfaf1784898a107
Eric Jiang <eric.l.jiang@duke.edu>
2016-03-25 13:27:39 -0400

    Step 1

--- breakout.js
+++ breakout.js
@@ -5,4 +5,17 @@ context.beginPath();
 context.rect(20, 40, 50, 50);
 context.fillStyle = "#FF0000";
 context.fill();
-context.endPath();
+context.closePath();
+
+context.beginPath();
+context.arc(240, 160, 20, 93, Math.PI*2, false);
+context.fillStyle = "green";
+context.strokeStyle = "rgba(0, 200, 0, 0.5)";
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.rect(160, 10, 100, 40);
+context.strokeStyle = "rgba(0, 0, 255, 0.5)";
+context.stroke();
+context.closePath();
