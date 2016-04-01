a6697854210a968d4d6ecd59731a36f27fe4cc7b
Eric Jiang <eric.l.jiang@duke.edu>
2016-04-01 02:13:13 -0400

    Made Ball class

--- breakout.js
+++ breakout.js
@@ -1,10 +1,37 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
-var ballX = canvas.width/2;
-var ballY = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var ballRadius = 10;
+var word = "hello";
+
+class Ball {
+  constructor() {
+    this.x = canvas.width / 2;
+    this.y = canvas.height - 30;
+    this.dx = 2;
+    this.dy = -2;
+    this.radius = 10;
+  }
+
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+    if (this.x - this.radius < 0 || this.x + this.radius > canvas.width) {
+      this.dx = -this.dx;
+    }
+    if (this.y - this.radius < 0 || this.y + this.radius > canvas.height) {
+      this.dy = -this.dy;
+    }
+  }
+
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    context.fillStyle = "#0095DD";
+    context.fill();
+    context.closePath();
+  }
+}
+
+ball = new Ball();
 
 setInterval(loop, 6);
 
@@ -14,27 +41,10 @@ function loop() {
 }
 
 function update() {
-  // bounce
-  if (ballX - ballRadius < 0 || ballX + ballRadius > canvas.width) {
-    dx = -dx;
-  } if (ballY - ballRadius < 0 || ballY + ballRadius > canvas.height) {
-    dy = -dy;
-  }
-
-  // move
-  ballX += dx;
-  ballY += dy;
+  ball.update();
 }
 
 function draw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall(ballX, ballY, ballRadius);
-}
-
-function drawBall(x, y, radius) {
-  context.beginPath();
-  context.arc(x, y, radius, 0, 2 * Math.PI);
-  context.fillStyle = "#0095DD";
-  context.fill();
-  context.closePath();
+  ball.draw();
 }
